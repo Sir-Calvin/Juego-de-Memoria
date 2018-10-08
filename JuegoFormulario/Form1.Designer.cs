cac758b6bea2280afdfc1f9727a3fd3938767559
Emiliano Calvacho <39678652+Sir-Calvin@users.noreply.github.com>
2018-10-08 16:38:40 -0300

    Add files via upload

---
+++ JuegoFormulario/Form1.Designer.cs
@@ -0,0 +1,274 @@
+﻿namespace JuegoFormulario
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.juegoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.iniciarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.terminarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timerMostrarSec = new System.Windows.Forms.Timer(this.components);
+            this.timerApagarBtns = new System.Windows.Forms.Timer(this.components);
+            this.timerRespuesta = new System.Windows.Forms.Timer(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.labelTiempo = new System.Windows.Forms.Label();
+            this.labelSegundero = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.MediumPurple;
+            this.button6.Location = new System.Drawing.Point(27, 215);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(121, 110);
+            this.button6.TabIndex = 0;
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.buttonVioleta_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.button1.Location = new System.Drawing.Point(27, 40);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 110);
+            this.button1.TabIndex = 1;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.buttonAzul_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.button2.Location = new System.Drawing.Point(196, 40);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(121, 110);
+            this.button2.TabIndex = 2;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.buttonRojo_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.button3.Location = new System.Drawing.Point(364, 40);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(121, 110);
+            this.button3.TabIndex = 3;
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.buttonNaranja_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.button5.Location = new System.Drawing.Point(196, 215);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(121, 110);
+            this.button5.TabIndex = 4;
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.buttonVerde_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.button4.Location = new System.Drawing.Point(364, 215);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(121, 110);
+            this.button4.TabIndex = 5;
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.buttonAmarillo_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.juegoToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(512, 24);
+            this.menuStrip1.TabIndex = 8;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // juegoToolStripMenuItem
+            // 
+            this.juegoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.iniciarToolStripMenuItem,
+            this.terminarToolStripMenuItem});
+            this.juegoToolStripMenuItem.Name = "juegoToolStripMenuItem";
+            this.juegoToolStripMenuItem.Size = new System.Drawing.Size(56, 20);
+            this.juegoToolStripMenuItem.Text = "Partida";
+            // 
+            // iniciarToolStripMenuItem
+            // 
+            this.iniciarToolStripMenuItem.Name = "iniciarToolStripMenuItem";
+            this.iniciarToolStripMenuItem.Size = new System.Drawing.Size(121, 22);
+            this.iniciarToolStripMenuItem.Text = "Iniciar";
+            this.iniciarToolStripMenuItem.Click += new System.EventHandler(this.iniciarToolStripMenuItem_Click);
+            // 
+            // terminarToolStripMenuItem
+            // 
+            this.terminarToolStripMenuItem.Name = "terminarToolStripMenuItem";
+            this.terminarToolStripMenuItem.Size = new System.Drawing.Size(121, 22);
+            this.terminarToolStripMenuItem.Text = "Terminar";
+            this.terminarToolStripMenuItem.Click += new System.EventHandler(this.terminarToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(101, 164);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(10, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "-";
+            this.label1.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(101, 191);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(10, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "-";
+            this.label2.Visible = false;
+            // 
+            // timerMostrarSec
+            // 
+            this.timerMostrarSec.Interval = 1000;
+            this.timerMostrarSec.Tick += new System.EventHandler(this.timerMostrarSec_Tick);
+            // 
+            // timerApagarBtns
+            // 
+            this.timerApagarBtns.Interval = 1000;
+            this.timerApagarBtns.Tick += new System.EventHandler(this.timerApagarBtns_Tick);
+            // 
+            // timerRespuesta
+            // 
+            this.timerRespuesta.Interval = 1000;
+            this.timerRespuesta.Tick += new System.EventHandler(this.timerRespuesta_Tick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 164);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Secuencias";
+            this.label3.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 191);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(83, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Secuencia User";
+            this.label4.Visible = false;
+            // 
+            // labelTiempo
+            // 
+            this.labelTiempo.AutoSize = true;
+            this.labelTiempo.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTiempo.Location = new System.Drawing.Point(162, 337);
+            this.labelTiempo.Name = "labelTiempo";
+            this.labelTiempo.Size = new System.Drawing.Size(145, 16);
+            this.labelTiempo.TabIndex = 12;
+            this.labelTiempo.Text = "Tiempo de Respuesta";
+            // 
+            // labelSegundero
+            // 
+            this.labelSegundero.AutoSize = true;
+            this.labelSegundero.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelSegundero.ForeColor = System.Drawing.Color.Red;
+            this.labelSegundero.Location = new System.Drawing.Point(322, 337);
+            this.labelSegundero.Name = "labelSegundero";
+            this.labelSegundero.Size = new System.Drawing.Size(14, 17);
+            this.labelSegundero.TabIndex = 13;
+            this.labelSegundero.Text = "-";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(512, 363);
+            this.Controls.Add(this.labelSegundero);
+            this.Controls.Add(this.labelTiempo);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem juegoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem iniciarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem terminarToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timerMostrarSec;
+        private System.Windows.Forms.Timer timerApagarBtns;
+        private System.Windows.Forms.Timer timerRespuesta;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label labelTiempo;
+        private System.Windows.Forms.Label labelSegundero;
+    }
+}
+
---
+++ JuegoFormulario/Form1.cs
@@ -0,0 +1,377 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JuegoFormulario
+{
+    public partial class Form1 : Form
+    {
+        Juego jugando; // Uso la clase con la logica del juego
+        List<int> secActual; // Guardare la ultima secuencia generada
+        Color[] colores = new Color[] { Color.FromArgb(128, 128, 255),
+                                        Color.FromArgb(255, 128, 128),
+                                        Color.FromArgb(255, 192, 128),
+                                        Color.FromArgb(255, 255, 128),
+                                        Color.FromArgb(128, 255, 128),
+                                        Color.MediumPurple}; // Contiene la lista de colores
+        int tiempoDeRespuesta; // Es el tiempo que tiene el usuario para responder
+
+        #region Constructores
+        public Form1()
+        {
+            InitializeComponent();
+            CambioBotones(false); 
+        }
+
+        public void Incializador()
+        {
+            jugando = new Juego();
+            secActual = new List<int>();
+            tiempoDeRespuesta = 10;
+            HacerColoresAleatorios();
+        }
+        #endregion
+
+        #region MenuStrip
+        /** Estas funciones contienen el desarrollo de las opciones del menu strip **/
+
+        // Inciar el juego con parametros por defecto y comienza con una serie
+        private void iniciarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (TomarDecision("INICIAR"))
+            {
+                Incializador();
+                HacerSerie();
+                label1.Text = jugando.MostrarListaSecuencias();
+            }
+        }
+
+        // Termina el juego saliendo del formulario
+        private void terminarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (TomarDecision("TERMINAR"))
+            {
+                Application.Exit();
+            }
+        }
+        #endregion
+
+        #region Funcion Auxiliar
+        /* TomarDecision: es una funcion que de acuerdo al string correspondiente devuelve
+         * true o false para Inciar o Salir del juego según la decisión del dialogo result
+         */
+        public bool TomarDecision(string decision)
+        {
+            DialogResult dr = MessageBox.Show("¿Quiere " + decision + " el juego?", decision + " juego", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+            if (DialogResult.Yes == dr)
+            {
+                return true;
+            }
+            return false;
+        }
+        #endregion
+
+        #region Colores de Botones
+        /** Estas funciones permiten cambiar el color de los botones y habiliatarlos**/
+
+        /* CambioBotones: habilita los botones segun el parametro bool que se le pase y
+         * en ambos casos de que sea true o false los pinta de blanco a todos los botones
+         */
+        public void CambioBotones(bool activar)
+        {
+            foreach(Button btn in Controls.OfType<Button>())
+            {
+                if (activar)
+                {
+                    btn.Enabled = true;
+                }
+                else
+                {
+                    btn.Enabled = false;
+                }
+                btn.BackColor = Color.White;
+            }
+        }
+
+        /* HacerColoresAleatorios: permite que cada vez que se inicie el juego
+         * se cambie de lugar los colores de los botones.
+         */
+        public void HacerColoresAleatorios()
+        {
+            Random rnd = new Random();
+            List<int> coloresElegidos = new List<int>();
+            int numero = rnd.Next(0, 6);
+            coloresElegidos.Add(numero);
+            for(int i = 0; i < 5; i++)
+            {
+                do 
+                { 
+                    numero = rnd.Next(0, 6);
+                } while(coloresElegidos.Contains(numero));
+                coloresElegidos.Add(numero);
+            }
+            Color aux = new Color();
+            int cont = 0;
+            foreach(int item in coloresElegidos)
+            {
+                aux = colores[cont];
+                colores[cont] = colores[item];
+                colores[item] = aux;
+            }
+        }
+
+        /* PintarBotones: pinta el boton segun el numero de indice que se le pase y ademas
+         * llama a un timer para despintarlos segun sea necesario
+         */
+        public void PintarBotones(int item)
+        {
+            switch (item)
+            {
+                case 1:
+                    button1.BackColor = colores[0];
+                    timerApagarBtns.Start();
+                    break;
+                case 2:
+                    button2.BackColor = colores[1];
+                    timerApagarBtns.Start();
+                    break;
+                case 3:
+                    button3.BackColor = colores[2];
+                    timerApagarBtns.Start();
+                    break;
+                case 4:
+                    button4.BackColor = colores[3];
+                    timerApagarBtns.Start();
+                    break;
+                case 5:
+                    button5.BackColor = colores[4];
+                    timerApagarBtns.Start();
+                    break;
+                case 6:
+                    button6.BackColor = colores[5];
+                    timerApagarBtns.Start();
+                    break;
+            }
+        }
+
+        /*
+        public void PintarBotones(int item)
+        {
+
+            switch (item)
+            {
+                case 1:
+                    button1.BackColor = Color.FromArgb(128, 128, 255);
+                    timerApagarBtns.Start();
+                    break;
+                case 2:
+                    button2.BackColor = Color.FromArgb(255, 128, 128);
+                    timerApagarBtns.Start();
+                    break;
+                case 3:
+                    button3.BackColor = Color.FromArgb(255, 192, 128);
+                    timerApagarBtns.Start();
+                    break;
+                case 4:
+                    button4.BackColor = Color.FromArgb(255, 255, 128);
+                    timerApagarBtns.Start();
+                    break;
+                case 5:
+                    button5.BackColor = Color.FromArgb(128, 255, 128);
+                    timerApagarBtns.Start();
+                    break;
+                case 6:
+                    button6.BackColor = Color.MediumPurple;
+                    timerApagarBtns.Start();
+                    break;
+            }
+        }*/
+        #endregion
+
+        #region Logica del Juego en el Formulario
+        /** Estas funciones van a utilizar la logica de la clase juego y de la variable
+            jugando, influyendo en los mensajes al usuario y activando los timer y Message
+            Boxs correspondientes. **/
+
+        /* HacerSerie: permite armar la secuencia siempre y cuando el juego no llegue al
+         * nivel 7. Estas secuencias aleatorias al guardarse en una lista de secuencias, 
+         * siempre es conveniente ver y poder utilizar la ultima secuencia entonces a la
+         * secuencia actual guardamos la ultima secuencia de lista de secuencias del jugando.
+         * Además inicializa el timer de Mostrar Secuencias para pintar los botones correctos.
+         */
+        public void HacerSerie()
+        {
+            if (jugando.Niveles < 7)
+            {
+                jugando.ArmarSecuencia();
+                secActual = jugando.ListaDeSecuencia[jugando.CantSec() - 1];
+                timerMostrarSec.Start();
+            }    
+        }
+
+        /* VerificarBotones: si la cantidad de enteros en la secuencia generada es distinta
+         * a la del usuario entonces espero hasta que sea igual. Cuando sean iguales entonces
+         * guardo en una variable auxiliar el resultado de la comparacion de estas secuencias
+         * si este es true y los niveles son menor a 6 entonces el usuario acerto y continua, 
+         * pero si el aux es falso entonces el usuario no acerto y pierde, y en el caso que
+         * se haya llegado a los 6 niveles acertando se muestra el mensaje de Ganador.
+         * Frena el tiempo de respuesta y resetea su label
+         */
+        public void VerificarBotones()
+        {
+            if (jugando.CantSec()==jugando.CantUsuar())
+            {
+                bool band = jugando.CompararSecuencias();
+                timerRespuesta.Stop();
+                labelSegundero.Text = "-";
+
+                if (band && jugando.Niveles < 6)
+                {
+                    MessageBox.Show("Has acertado, continuemos...", "Acertado", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    HacerSerie();
+                    jugando.LimpiarSecuencias();
+                    label1.Text = jugando.MostrarListaSecuencias();
+                    label2.Text = "-";
+                    tiempoDeRespuesta = 10;
+                }
+                else if(!band)
+                {
+                    MessageBox.Show("Has perdido, lo siento :(", "Game Over", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    CambioBotones(false);
+                    label1.Text = "-";
+                    label2.Text = "-";
+                }
+                else
+                {
+                    MessageBox.Show("Has ganado, felicitaciones :)", "Ganaste", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    CambioBotones(false);
+                    label1.Text = "-";
+                    label2.Text = "-";
+                }     
+            }
+        }
+
+
+        #endregion
+
+        #region Decisiones sobre los Botones de Colores
+        /** Cada uno de los botones tiene la logica de agregar segun le corresponda su
+            numero entero a la secuencia del usuario, mostrar (opcionalmente) dicha 
+            secuencia y verificar que la secuencia sea la misma que la generada.**/
+        private void buttonAzul_Click(object sender, EventArgs e)
+        {
+            jugando.SecuenciaUsuario(1);
+            label2.Text = jugando.MostrarListaUsuario();
+            VerificarBotones();
+        }
+
+        private void buttonRojo_Click(object sender, EventArgs e)
+        {
+            jugando.SecuenciaUsuario(2);
+            label2.Text = jugando.MostrarListaUsuario();
+            VerificarBotones();
+        }
+
+        private void buttonNaranja_Click(object sender, EventArgs e)
+        {
+            jugando.SecuenciaUsuario(3);
+            label2.Text = jugando.MostrarListaUsuario();
+            VerificarBotones();
+        }
+
+        private void buttonAmarillo_Click(object sender, EventArgs e)
+        {
+            jugando.SecuenciaUsuario(4);
+            label2.Text = jugando.MostrarListaUsuario();
+            VerificarBotones();
+        }
+
+        private void buttonVerde_Click(object sender, EventArgs e)
+        {
+            jugando.SecuenciaUsuario(5);
+            label2.Text = jugando.MostrarListaUsuario();
+            VerificarBotones();
+        }
+
+        private void buttonVioleta_Click(object sender, EventArgs e)
+        {
+            jugando.SecuenciaUsuario(6);
+            label2.Text = jugando.MostrarListaUsuario();
+            VerificarBotones();
+        }
+        #endregion
+
+        #region Logica de timers
+        int numeroSec = 0; // Para avanzar segun el tiempo
+
+        /* El tick del timerMostrarSec verifica que si el numero de secuencia es menor
+         * a la cantidad de la secuencia actual pinte los botones segun la posicion de
+         * dicho numero, incremente ese numero y haga que el intervalo de tiempo se aumente
+         * en 100 ms. Pero si no es menor el numero de secuencia a la cantidad de la secuencia
+         * actual entonces debera iniciarse en cero el numero, se reestablece el valor del
+         * intervalo en 1000 ms y se frena el tick.
+         */
+        private void timerMostrarSec_Tick(object sender, EventArgs e)
+        {
+            if(numeroSec < secActual.Count())
+            {
+                PintarBotones(secActual[numeroSec]);
+                numeroSec++;
+                timerMostrarSec.Interval += 100;
+            }
+            else
+            {
+                numeroSec = 0;
+                timerMostrarSec.Interval = 1000;
+                timerMostrarSec.Stop();
+            }
+        }
+
+        /* El tick del timerApagarBtns verifica que si el numero de secuencia es igual
+         * a la cantidad de la secuencia actual, habilite los botones para tocar y sino 
+         * no deja que los botones queden habilitados. En ambos casos se detiene el tick.
+         * Cuando se ha concretado la secuencia comienza el tercer timer de respuesta,
+         * para que el usuario tenga la dificultad al responder.
+         */
+        private void timerApagarBtns_Tick(object sender, EventArgs e)
+        {
+            if(numeroSec == secActual.Count())
+            {
+                CambioBotones(true);
+                timerRespuesta.Start();
+                timerApagarBtns.Stop();
+            }
+            else
+            {
+                CambioBotones(false);
+                timerApagarBtns.Stop();
+            }
+        }
+        
+        /* El tick de timerRespuesta me sirve para contar 10 segundos en cada respuesta
+         * en caso de no cumplir, mandar un mensaje de tiempo acabado
+         */
+        private void timerRespuesta_Tick(object sender, EventArgs e)
+        {
+            if (tiempoDeRespuesta == 0)
+            {
+                labelSegundero.Text = "-"; 
+                timerRespuesta.Stop();     
+                MessageBox.Show("Se le acabó el tiempo", "Tiempo fuera", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                CambioBotones(false);
+            }
+            else
+            {
+                labelSegundero.Text = tiempoDeRespuesta.ToString();
+                tiempoDeRespuesta--;
+            }
+        }
+        #endregion
+    }
+}
